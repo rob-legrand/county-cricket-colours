5d0bbd454d2df733c70d5c2a8983d4150ea13db7
Rob LeGrand <roblegrand98@gmail.com>
2025-12-11 17:21:42 -0600

    Add 'countyName' as a value for textType option in createCountyElement method

--- counties.js
+++ counties.js
@@ -2563,6 +2563,8 @@ const counties = (function () {
                   || Number.isFinite(options?.textContent)
                )
                ? options.textContent
+               : options?.textType === 'countyName'
+               ? options.county?.countyName ?? ''
                : options?.textType === 'countyNameInWelsh'
                ? options.county?.countyNameInWelsh ?? options.county?.countyName ?? ''
                : options?.textType === 'countyAbbreviation'
