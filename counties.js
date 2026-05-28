e9af1747ab566025302a151e6200c7dad6293a29
Rob LeGrand <roblegrand98@gmail.com>
2026-05-28 17:41:39 -0500

    Rename mud property to coffee

--- counties.js
+++ counties.js
@@ -381,10 +381,10 @@ const counties = (function () {
       ),
       namedColours: {
          black: [0, 0, 0],
+         coffee: [60, 50, 30],
          cream: [255, 245, 225],
          darkGrey: [75, 75, 75],
          lightGrey: [180, 180, 180],
-         mud: [60, 50, 30],
          white: [255, 255, 255]
       }
    });
